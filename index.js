610879220aee3e1d82e7def3dcf7f2008ff112c2
Cristiambustos <cristiambus@gmail.com>
2021-01-23 21:22:44 -0500

    1.2 Model sequelize

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+
+
 const bodyParser = require('body-parser');
+
 
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-    res.send('Estamos conectado compaaaa!')
-});
-
-app.set('port', process.env.PORT || 4000);
-
-app.listen(app.get('port'), () => {
-    console.log('Server on port ' + app.get('port') + ' on dev');
+    res.send('Conectado campeón!')
 })
+app.set('port', process.env.PORT || 3000);
+
+    app.listen(app.get('port'), () => {
+        console.log('Server on port ' + app.get('port') + ' on dev');
+    });
+
+
